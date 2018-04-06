603120a78a9bf5ef79d75337e368ec3184bf683d
Nathan <nathan_mensah@my.uri.edu>
2018-04-05 21:11:19 -0400

    Fixed Picture switch

--- Python/SwitchImage.py
+++ Python/SwitchImage.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
-
+import tempfile
 import os
 from random import choice
+
 ext2conttype = {
 "jpg": "image/jpeg",
 "jpeg": "image/jpeg",
@@ -23,11 +24,27 @@ def random_file(dir):
     #print images
 def pick_file(dir):
     return null
+def randomval():
+    file = open('./outputFile.txt', 'r')
+    a = sum(map(int, file.read(2)))
+    if ((a) % 3 == 2):
+        return 2
+    elif ((a) % 3 == 1):
+        return 1
+    else:
+        return 0
+
+def pair(val, dir):
+    images = [f for f in os.listdir(os.path.expanduser(dir)) ]
+    return images[val]
 
 if __name__ == "__main__":
     #dir = "../HTML/Images/"
-
+    #print randomval()
     dir = "~/Documents/Images/"
-    r = random_file(dir)
+    r = pair (randomval(), dir)
+
+    #print dir
+    #r = random_file(dir)
     print r
     #print "Content-type: %s\n" %(content_type(r))
